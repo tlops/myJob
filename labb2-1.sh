11c5d1d87fd431195ea5098b7f01a7fcb78eec37
tlops <muyiwafalade@hotmail.com>
2013-12-09 13:24:35 +0100

    testing user input

---
+++ labb2-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -n "Vad heter du?"
+    read Username
+    echo -n "Hur gammal är du?"
+    read Age
+    echo -n "Var bor du?"
+    read Ort
+echo "Du är $Username, du är $Age år gammal och bor i $Ort!"
