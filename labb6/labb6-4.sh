a3208bcd76e9d0ae9fe0c345e7b4da97431e603a
tlops <muyiwafalade@hotmail.com>
2013-12-22 15:41:52 +0100

    Laboration 6

--- labb6/labb6-4.sh
+++ labb6/labb6-4.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 #
 #
-function add { 
-    sum=$[ $Nr1 + $Nr2 ]
-    return $sum
+function add {
+    sum=$(( $Nr1 + $Nr2 ))
+    echo "$Nr1 + $Nr2 = $sum"
 }
 
-read -p "Enter two Numbers" Nr1 Nr2
-echo "$add"
+function minus {
+    diff=$(( $Nr1 - $Nr2 ))
+    echo "$Nr1 - $Nr2 = $diff"
+}
+
+read -p "Enter two Numbers: " Nr1 Nr2
+add $Nr1 $Nr2
+
+read -p "Enter two Numbers: " Nr1 Nr2
+minus $Nr1 $Nr2
+echo $# $*
 
